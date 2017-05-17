46dd020c210b70f74833819820bef9dd2af27c88
harmannd <derekhar2468@gmail.com>
2017-05-17 13:21:46 -0500

    Fixed uppercase letters

--- main.py
+++ main.py
@@ -6,22 +6,33 @@ template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
                                autoescape = True)
 
-alphabet = ['a', 'b', 'c', 'd', 'e',
-            'f', 'g', 'h', 'i', 'j',
-            'k', 'l', 'm', 'n', 'o',
-            'p', 'q', 'r', 's', 't',
-            'u', 'v', 'w', 'x', 'y',
-            'z']
+low_alphabet = ['a', 'b', 'c', 'd', 'e',
+                'f', 'g', 'h', 'i', 'j',
+                'k', 'l', 'm', 'n', 'o',
+                'p', 'q', 'r', 's', 't',
+                'u', 'v', 'w', 'x', 'y',
+                'z']
+cap_alphabet = ['A', 'B', 'C', 'D', 'E',
+                'F', 'G', 'H', 'I', 'J',
+                'K', 'L', 'M', 'N', 'O',
+                'P', 'Q', 'R', 'S', 'T',
+                'U', 'V', 'W', 'X', 'Y',
+                'Z']
 
 def rot13(str):
     str_list = list(str)
     for i in xrange(len(str_list)):
-        if str_list[i] in alphabet:
+        if str_list[i] in low_alphabet or str_list[i] in cap_alphabet:
+            if str_list[i] in cap_alphabet:
+                alphabet = cap_alphabet
+            else:
+                alphabet = low_alphabet
             char_index = alphabet.index(str_list[i])
             new_index = char_index + 13
             if new_index > 25:
                 new_index -= 26
             str_list[i] = alphabet[new_index]
+
 
     return ''.join(str_list)
 
